435e49e10a82c5d1f6f665fda1d0cb541866d465
Sergio Calvo <scalvogarcia95@gmail.com>
2018-03-12 00:01:43 +0100

    el menu de los huevos

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,50 +1,156 @@
 package main;
 
+import java.util.Calendar;
 import java.util.Scanner;
+
+import cliente.Cliente;
+import cliente.Direccion;
+import factura.Llamada;
+import factura.Tarifa;
 import gestion.Gestion;
 
 public class Main {
 	public static void main(String[] args) {
 		Gestion gestion = new Gestion();			
 		System.out.println(Menu.getMenu());
-		Scanner scanner = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Elige una opción:");
-		byte opcion = scanner.nextByte();
+		byte opcion = scan.nextByte();
 		Menu menu = Menu.getOpcion(opcion);
 		switch (menu) {
 		case ALTA_CLIENTE:
-			gestion.darDeAltaCliente(main.Consola.pedirDatosCliente());
+			System.out.println("Introducir datos cliente: ");
+			System.out.print("Introduce nombre: ");
+			String nombre = scan.next();
+			System.out.print("Introduce NIF: ");
+			String nif = scan.next();
+			System.out.println("Introduce dirección: ");
+			System.out.print("	-Código Postal: ");
+			int codP = scan.nextInt();
+			System.out.print("	-Provincia: ");
+			String prov = scan.next();
+			System.out.print("	-Población: ");
+			String pob = scan.next();
+			Direccion dir = new Direccion(codP,prov,pob);
+			System.out.print("Introduce correo: ");
+			String correo = scan.next();
+			System.out.println("Introduce fecha de alta: ");
+			System.out.print("	-Año: ");
+			int año = scan.nextInt();
+			System.out.print("	-Mes (numérico): ");
+			int mes = scan.nextInt();
+			System.out.print("	-Día: ");
+			int dia = scan.nextInt();
+			Calendar fecha = Calendar.getInstance();
+			fecha.set(año, mes, dia);
+			System.out.print("Introduce tarifa: ");
+			Tarifa tarifa = new Tarifa(scan.nextDouble());
+			Cliente cliente = new Cliente(nombre,nif,dir,correo,fecha,tarifa);
+			gestion.darDeAltaCliente(cliente);
 			break;
 		case BORRAR_CLIENTE:
-			gestion.borrarCliente(main.Consola.pedirDatosCliente());
+			System.out.println("Introducir datos cliente: ");
+			System.out.print("Introduce nombre: ");
+			nombre = scan.next();
+			System.out.print("Introduce NIF: ");
+			nif = scan.next();
+			System.out.println("Introduce dirección: ");
+			System.out.print("	-Código Postal: ");
+			codP = scan.nextInt();
+			System.out.print("	-Provincia: ");
+			prov = scan.next();
+			System.out.print("	-Población: ");
+			pob = scan.next();
+			dir = new Direccion(codP,prov,pob);
+			System.out.print("Introduce correo: ");
+			correo = scan.next();
+			System.out.println("Introduce fecha de alta: ");
+			System.out.print("	-Año: ");
+			año = scan.nextInt();
+			System.out.print("	-Mes (numérico): ");
+			mes = scan.nextInt();
+			System.out.print("	-Día: ");
+			dia = scan.nextInt();
+			Calendar fecha2 = Calendar.getInstance();
+			fecha2.set(año, mes, dia);
+			System.out.print("Introduce tarifa: ");
+			tarifa = new Tarifa(scan.nextDouble());
+			cliente = new Cliente(nombre,nif,dir,correo,fecha2,tarifa);
+			gestion.borrarCliente(cliente);
 			break;
 		case CAMBIAR_TARIFA:
-			gestion.cambiarTarifa(main.Consola.pedirNIF(), main.Consola.pedirTarifa());
+			System.out.print("Introduce NIF: ");
+			nif = scan.next();
+			System.out.print("Introduce tarifa: ");
+			tarifa = new Tarifa(scan.nextDouble());
+			gestion.cambiarTarifa(nif, tarifa);
 			break;
 		case RECUPERAR_CLIENTE_NIF:
-			gestion.recuperarDatosNIF(main.Consola.pedirNIF());
+			System.out.print("Introduce NIF: ");
+			nif = scan.next();
+			gestion.recuperarDatosNIF(nif);
 			break;
 		case RECUPERAR_TODOS:
 			gestion.recuperarListadoClientes();
 			break;
 		case DAR_ALTA_LLAMADA:
-			gestion.darDeAltaLlamada(main.Consola.pedirNIF(), main.Consola.pedirLlamada());
+			System.out.print("Introduce NIF: ");
+			nif = scan.next();
+			System.out.print("Introduce el número de teléfono: ");
+			int num =scan.nextInt();
+			System.out.println("Introduce fecha: ");
+			System.out.print("	-Año: ");
+			año = scan.nextInt();
+			System.out.print("	-Mes (numérico): ");
+			mes = scan.nextInt();
+			System.out.print("	-Día: ");
+			dia = scan.nextInt();
+			fecha = Calendar.getInstance();
+			fecha.set(año, mes, dia);
+			System.out.print("Introduce hora: ");
+			int hor = scan.nextInt();
+			System.out.print("Introduce minuto: ");
+			int min = scan.nextInt();
+			Calendar hora = Calendar.getInstance();
+			hora.set(año, mes, dia, hor, min);
+			System.out.print("Introduce duración: ");
+			int dur=scan.nextInt();
+			
+			Llamada llamada = new Llamada(num, fecha, hora, dur);
+			gestion.darDeAltaLlamada(nif, llamada);
 			break;
 		case LISTAR_LLAMADAS:
-			gestion.listarLlamadasCliente(main.Consola.pedirNIF());
+			System.out.print("Introduce NIF: ");
+			nif = scan.next();
+			gestion.listarLlamadasCliente(nif);
 			break;
 		case EMITIR_FACTURA:
-			gestion.emitirFactura(main.Consola.pedirNIF(), main.Consola.pedirFecha());
+			System.out.print("Introduce NIF: ");
+			nif = scan.next();
+			System.out.println("Introduce fecha: ");
+			System.out.print("	-Año: ");
+			año = scan.nextInt();
+			System.out.print("	-Mes (numérico): ");
+			mes = scan.nextInt();
+			System.out.print("	-Día: ");
+			dia = scan.nextInt();
+			fecha = Calendar.getInstance();
+			fecha.set(año, mes, dia);
+			gestion.emitirFactura(nif, fecha);
 			break;
 		case RECUPERAR_DATOS_FACTURA:
-			gestion.recuperarDatosFacturaCodigo(main.Consola.pedirCod());
+			System.out.print("Introduce código de factura: ");
+			String cod = scan.next();
+			gestion.recuperarDatosFacturaCodigo(cod);
 			break;
 		case RECUPERAR_FACTURAS:
-			gestion.recuperarFacturas(main.Consola.pedirNIF());
+			System.out.print("Introduce NIF: ");
+			nif = scan.next();
+			gestion.recuperarFacturas(nif);
 		default:
 			break;
 		}
 		System.out.println("Ok");
-		scanner.close();
+		scan.close();
 	}
 }
