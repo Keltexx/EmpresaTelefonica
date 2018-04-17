fc4112733dbfcbcb9e1903b59f81fe605e011727
al315612 <al315612@uji.es>
2018-04-17 18:54:04 +0200

    test

---
+++ src/test/java/tarifas/FactoryTest.java
@@ -0,0 +1,62 @@
+package tarifas;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cliente.ClienteFactory;
+import cliente.Direccion;
+import cliente.Empresa;
+import cliente.Particular;
+import factura.TarifaBasica;
+
+
+public class FactoryTest {
+
+	private Empresa empresa;
+	private Particular particular;
+	private TarifaBasica tarifa = new TarifaBasica(5);
+	private String nombre = "Pepe";
+	private String nif = "0001";
+	private int cod = 1;
+	private String prov = "Castellon";
+	private String pob = "cs";
+	private Direccion dir = new Direccion(cod, prov, pob);
+	private String correo = "jaja";
+	private Calendar fecha = Calendar.getInstance();
+	private String apellidos = "Tenas";
+
+	
+	@Before
+	public void init() {
+		empresa = new Empresa(nombre, nif, dir, correo, fecha, tarifa);
+		particular = new Particular(nombre, apellidos, nif, dir, correo, fecha, tarifa);
+		
+	}
+	
+	@After
+	public void finish() {
+		empresa = null;
+		particular = null; 
+		tarifa=null;
+	}
+
+	@Test
+	public void testCliente() {
+        Empresa empresa2 = (Empresa) ClienteFactory.crearCliente(0, nombre, apellidos, nif, dir, correo, fecha, tarifa);
+        assertEquals(empresa.getNombre(),empresa2.getNombre());
+        assertEquals(empresa.getCorreo(),empresa2.getCorreo());
+        assertEquals(empresa.getNIF(),empresa2.getNIF());
+        
+        Particular particular2 = (Particular) ClienteFactory.crearCliente(1, nombre, apellidos, nif, dir, correo, fecha, tarifa);
+        assertEquals(particular.getNombre(),particular2.getNombre());
+        assertEquals(particular.getApellidos(),particular2.getApellidos());
+        assertEquals(particular.getCorreo(),particular2.getCorreo());
+
+	}
+
+}
