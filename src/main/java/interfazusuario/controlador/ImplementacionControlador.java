f6f871bf2c9ebf6947c67799792c182cd5b9f2fb
Sergio <al286319@uji.es>
2018-05-19 14:01:04 +0200

    lfdkn

--- src/main/java/interfazusuario/controlador/ImplementacionControlador.java
+++ src/main/java/interfazusuario/controlador/ImplementacionControlador.java
@@ -2,4 +2,19 @@ package interfazusuario.controlador;
 
 public class ImplementacionControlador implements Controlador{
 
+	public void creaCliente() {
+		Cliente cliente = 
+	}
+	
+	public void borraCliente() {
+		
+	}
+	
+	public void cambiaTarifa() {
+		
+	}
+	
+	public void recuperarDatosCliente() {
+		
+	}
 }
